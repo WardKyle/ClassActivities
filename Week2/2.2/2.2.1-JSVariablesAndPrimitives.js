8b062a48878d61953a7e810e87f90281ace99b09
Kyle Ward <wardkyle77@gmail.com>
2023-08-30 19:28:03 -0500

    JS Variables and Primitives Practice 1

---
+++ Week2/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,24 @@
+// create a constant variable (const)
+ const constVariable = true;
+// create a variable that can be reassigned (let)
+let newLet;
+// create three variables and assign them values of different data types (=)
+let newString = "String";
+let newNumb = 1;
+let newBoolean = true;
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof newString, typeof newNumb, typeof newBoolean);
+// change the value referenced by a variable (dynamic typing)
+newNumb = "1";
+// print the type of the variable you just changed (typeof)
+console.log(typeof newNumb);
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+let myName = "Kyle Ward";
+let newLanguage = "Full Stack Web Development";
+let exp = 8;
+let master = false;
+let growthMindset = "always learning";
+console.log(`Hello, my name is ${myName}, and I am learning ${newLanguage}. I have been practicing for ${exp} years. My status as a master is: ${master} because I am ${growthMindset}`);
